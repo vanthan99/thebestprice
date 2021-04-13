129348fe45a511cb3fac147f91b172260ed87af5
vanthan99 <vanthan.ad.it@gmail.com>
2021-04-13 20:32:44 +0700

    update swagger config

--- src/main/java/dtu/thebestprice/controllers/ProductController.java
+++ src/main/java/dtu/thebestprice/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package dtu.thebestprice.controllers;
 
 import dtu.thebestprice.services.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/product")
+@Api
 public class ProductController {
     @Autowired
     ProductService productService;
 
-    @GetMapping
+    @ApiOperation(value = "Lọc sản phẩm")
+    @GetMapping(value = "/filter")
     public ResponseEntity<Object> filter(
-            @RequestParam(name = "keyword", required = false) String keyword,
-            @RequestParam(name = "catId", required = false) Long catId,
+            @ApiParam(value = "Từ khóa cần lọc") @RequestParam(name = "keyword", required = false) String keyword,
+            @ApiParam(value = "Mã danh mục cần lọc") @RequestParam(name = "catId", required = false) Long catId,
             Pageable pageable
     ){
+
         return ResponseEntity.ok(productService.filter(keyword, catId, pageable));
     }
 }
