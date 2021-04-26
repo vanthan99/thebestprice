292445129b4d4e798dca9401c307af00efdffce0
vanthan99 <vanthan.ad.it@gmail.com>
2021-04-26 15:28:12 +0700

    update

--- src/main/java/dtu/thebestprice/services/Impl/SearchServiceImpl.java
+++ src/main/java/dtu/thebestprice/services/Impl/SearchServiceImpl.java
@@ -1,9 +1,11 @@
 package dtu.thebestprice.services.Impl;
 
 import dtu.thebestprice.converters.ProductConverter;
+import dtu.thebestprice.payload.response.LongProductResponse;
 import dtu.thebestprice.repositories.ProductRepository;
 import dtu.thebestprice.services.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,4 +16,8 @@ public class SearchServiceImpl implements SearchService {
     @Autowired
     ProductConverter productConverter;
 
+    @Override
+    public Page<LongProductResponse> filter(String keyword) {
+        return null;
+    }
 }
--- src/main/java/dtu/thebestprice/services/SearchService.java
+++ src/main/java/dtu/thebestprice/services/SearchService.java
@@ -1,10 +1,11 @@
 package dtu.thebestprice.services;
 
 
+import dtu.thebestprice.payload.response.LongProductResponse;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface SearchService {
-    
+    Page<LongProductResponse> filter(String keyword);
 }
