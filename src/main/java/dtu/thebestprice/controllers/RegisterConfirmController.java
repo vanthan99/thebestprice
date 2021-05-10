8bead81ddbd36cf425bc0bb49f4717023799b6df
vanthan99 <vanthan.ad.it@gmail.com>
2021-05-10 14:41:52 +0700

    update

--- src/main/java/dtu/thebestprice/controllers/RegisterConfirmController.java
+++ src/main/java/dtu/thebestprice/controllers/RegisterConfirmController.java
@@ -3,18 +3,15 @@ package dtu.thebestprice.controllers;
 import dtu.thebestprice.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/xac-nhan-email")
+@RequestMapping("/registerConfirm")
 public class RegisterConfirmController {
     @Autowired
     AuthService authService;
 
-    @GetMapping
+    @PostMapping
     public String registerConfirm(
             @RequestParam(value = "token",required = false) String token
     ){
